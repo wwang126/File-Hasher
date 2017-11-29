85d627621653f92d298aeb4615006eee5914e620
wwang126 <wavelet.wang@gmail.com>
2017-11-29 05:10:00 -0800

    added different formatting for hex values to remove leading 0x

--- fileHasher.py
+++ fileHasher.py
@@ -4,21 +4,24 @@ import glob
 import os
 import sys
 
-#file storage
+#list of file names in directory
 fileNames = []
 
 #list files that are mkv
 for filename in glob.glob('*.mkv'):
    fileNames.append(filename)
 
+toWrite = ""
 for filename in fileNames:
     print("Reading: ", filename)
+    toWrite += str(filename) + "\n"
     fileIn = open(filename,"rb")
     fileBinary = fileIn.read()
     fileIn.close();
     fileHash = 0
     #hash file with crc
     fileHash = zlib.crc32(fileBinary)
-    fileHex = hex(fileHash)
-    print("Numerical Hash: " ,fileHash)
-    print("Hex Hash: ", fileHex)
+    fileHex = format(fileHash, 'x')
+    toWrite += "Numerical Hash: "  + str(fileHash) + "\n"
+    toWrite += "Hex Hash: " + str(fileHex) + "\n"
+print (toWrite)
