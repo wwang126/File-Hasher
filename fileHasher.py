c81bef7f86699144051db060d3711fafcf487830
wwang126 <wavelet.wang@gmail.com>
2017-11-30 02:59:32 -0800

    changed format of program to prepare for adding args

--- fileHasher.py
+++ fileHasher.py
@@ -2,33 +2,40 @@ import hashlib
 import zlib
 import glob
 import os
-import sys
+import sys, getopt
 
-#list of file names in directory
-fileNames = []
+def sfvWriter():
+    #list of file names in directory
+    fileNames = []
 
-#list files that are mkv
-for filename in glob.glob('*.mkv'):
-   fileNames.append(filename)
+    #list files that are mkv
+    for filename in glob.glob('*.mkv'):
+       fileNames.append(filename)
 
-toDisplay = ""
-toWrite = ";SFV Test\n"
-for filename in fileNames:
-    print("Reading: ", filename)
-    toDisplay += str(filename) + "\n"
-    toWrite += str(filename)
-    fileIn = open(filename,"rb")
-    fileBinary = fileIn.read()
-    fileIn.close()
-    fileHash = 0
-    #hash file with crc
-    fileHash = zlib.crc32(fileBinary)
-    fileHex = format(fileHash, 'x')
-    toDisplay += "Numerical Hash: "  + str(fileHash) + "\n"
-    toDisplay += "Hex Hash: " + str(fileHex) + "\n"
-    toWrite += " " + str(fileHex) + "\n"
-print (toDisplay)
+    toDisplay = ""
+    toWrite = ";SFV Test\n"
+    for filename in fileNames:
+        print("Reading: ", filename)
+        toDisplay += str(filename) + "\n"
+        toWrite += str(filename)
+        fileIn = open(filename,"rb")
+        fileBinary = fileIn.read()
+        fileIn.close()
+        fileHash = 0
+        #hash file with crc
+        fileHash = zlib.crc32(fileBinary)
+        fileHex = format(fileHash, 'x')
+        toDisplay += "Numerical Hash: "  + str(fileHash) + "\n"
+        toDisplay += "Hex Hash: " + str(fileHex) + "\n"
+        toWrite += " " + str(fileHex) + "\n"
+    print (toDisplay)
 
-fileWrite = open("SFVtest.sfv" , "w+")
-fileWrite.write(toWrite)
-fileWrite.close()
+    fileWrite = open("SFVtest.sfv" , "w+")
+    fileWrite.write(toWrite)
+    fileWrite.close()
+
+def main():
+    sfvWriter()
+
+if __name__ == "__main__":
+    main()
